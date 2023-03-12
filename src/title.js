29317b8fd60d39bd73d0486f59825a8d8d257679
hauhv_zyyx <hauhv@zyyx.jp>
2023-03-12 15:21:07 +0700

    update

--- src/title.js
+++ src/title.js
@@ -6,52 +6,52 @@ const EDITOR = 2
 const ITEMS = [START, CONTROLS, EDITOR]
 
 export default class Title extends Body {
-  constructor (game) {
-    super(document.getElementById('title'))
-    this.game = game
-    this.items = [].slice.call(this.element.querySelectorAll('.menu .item'))
-    this.selected = START
-  }
-
-  keydown ({key}) {
-    switch (key) {
-      case 'ArrowUp':
-        this.selected -= 1
-        break
-      case 'ArrowDown':
-        this.selected += 1
-        break
-      case 'Enter':
-        this.choose()
-        break
+    constructor(game) {
+        super(document.getElementById('title'))
+        this.game = game
+        this.items = [].slice.call(this.element.querySelectorAll('.menu .item'))
+        this.selected = START
     }
-  }
 
-  choose () {
-    switch (this.selected) {
-      case START:
-        this.game.scene.index = 0
-        this.game.scene.paused = false
-        this.game.state = 'play'
-        break
-      case EDITOR:
-        this.game.state = 'edit'
-        break
-      case CONTROLS:
-        this.game.state = 'controls'
-        break
+    keydown({ key }) {
+        switch (key) {
+            case 'ArrowUp':
+                this.selected -= 1
+                break
+            case 'ArrowDown':
+                this.selected += 1
+                break
+            case 'Enter':
+                this.choose()
+                break
+        }
     }
-  }
 
-  get selected () {
-    return this._selected
-  }
+    choose() {
+        switch (this.selected) {
+            case START:
+                this.game.scene.index = 0
+                this.game.scene.paused = false
+                this.game.state = 'play'
+                break
+            case EDITOR:
+                this.game.state = 'edit'
+                break
+            case CONTROLS:
+                this.game.state = 'controls'
+                break
+        }
+    }
 
-  set selected (value) {
-    this._selected = Math.min(ITEMS.length - 1, Math.max(0, value || 0))
+    get selected() {
+        return this._selected
+    }
 
-    this.items.forEach((item, index) => {
-      item.classList.toggle('selected', index === this.selected)
-    })
-  }
+    set selected(value) {
+        this._selected = Math.min(ITEMS.length - 1, Math.max(0, value || 0))
+
+        this.items.forEach((item, index) => {
+            item.classList.toggle('selected', index === this.selected)
+        })
+    }
 }
